519bf4ca1a87054a6439527940cc4f23f50d1dbe
plugnburn <plugnburn@mailnesia.com>
2014-08-21 23:43:22 +0300

    added WatchCoder (https://gist.github.com/plugnburn/a0d977e87ebdc21b1abb) as CLI-only utility

--- airootfs/root/customize_airootfs.sh
+++ airootfs/root/customize_airootfs.sh
@@ -22,3 +22,4 @@ sed -i 's/#\(Storage=\)auto/\1volatile/' /etc/systemd/journald.conf
 systemctl set-default multi-user.target
 
 ln -s /opt/asgard/vmusic /usr/bin/vmusic
+ln -s /opt/asgard/watchcoder /usr/bin/watchcoder
